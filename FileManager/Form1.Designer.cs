74cd21eefff5b375b156d95b79c50cc947684e75
FelesFurva <kitten.solar@gmail.com>
2022-07-06 19:27:27 +0300

    Add project files.

---
+++ FileManager/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace FileManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(83, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create a file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(330, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 34);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Read File";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(210, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(179, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(79, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 22);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter File name:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(210, 150);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(500, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(83, 219);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(337, 109);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(79, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 22);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Enter text:";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(210, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Write text";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(449, 78);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(92, 34);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Delete File";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(568, 78);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(84, 34);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Copy File";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(685, 78);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 34);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "Rename";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(716, 609);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(77, 30);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "Open";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(33, 388);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(760, 215);
+            this.listView1.TabIndex = 14;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(32, 32);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 614);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 22);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Select directory";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(149, 616);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(561, 20);
+            this.textBox2.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 678);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ FileManager/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileManager
+{
+    public partial class Form1 : Form
+    {
+        List<string> ListFiles = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        
+        public static string fileName;
+        public string fileText;
+        string path;
+        string selectedPath;
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        string GetFilePath()
+        {
+            fileName = textBox1.Text;
+            path = selectedPath + "/" + fileName + ".txt";
+            return path;
+        }
+
+
+        void CreateFile(string newFilePath)
+        {
+            if (!File.Exists(newFilePath))
+            {
+                using (StreamWriter sw = File.CreateText(newFilePath))
+                MessageBox.Show("File created sucessfully!");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetFilePath();
+            CreateFile(path);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        string GetTextforFile()
+        {
+            fileText = textBox3.Text;
+            return fileText;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GetFilePath();
+            GetTextforFile();
+            WriteToFile(path, fileText);
+        }
+        void WriteToFile(string path, string fileText)
+        {
+            if (File.Exists(path))
+            {
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.WriteLine(fileText);
+                    MessageBox.Show("Text saved sucessfully!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("No such file");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            GetFilePath();
+            ReadFile(path);
+        }
+
+        void ReadFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string text = sr.ReadLine();
+                    richTextBox1.Text = text;
+                }
+            }
+            else
+            {
+                MessageBox.Show("No such file");
+            }
+        }
+
+        void CopyFile(string path)
+        {
+            if(File.Exists(path))
+            {
+                
+                string copyName = fileName + "copy.txt";
+
+                string[] files = System.IO.Directory.GetFiles(selectedPath, fileName+"*");
+
+                int count = files.Count();
+                if (count > 1)
+                {
+                    copyName = fileName + $"copy{count}.txt";
+                }
+                string nameWithPath = selectedPath + "/" + copyName;
+                File.Copy(path, nameWithPath);
+                MessageBox.Show("File copied successfully!");
+            }
+            else
+            {
+                MessageBox.Show("No such file");
+            }
+        }
+
+        void RenameFile(string path)
+        {
+            if(File.Exists(path))
+            {
+                string newName = selectedPath +"/"+ textBox3.Text + ".txt";
+                File.Move(path, newName);
+                MessageBox.Show($"File {fileName} renamed {textBox3.Text}");
+            }
+            else
+            {
+                MessageBox.Show("No such file");
+            }
+
+        }
+
+        void DeleteFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+                MessageBox.Show("File deleted successfully");
+            }
+            else
+            {
+                MessageBox.Show("No such file");
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            GetFilePath();
+            DeleteFile(path);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            GetFilePath();
+            CopyFile(path);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            GetFilePath();
+            RenameFile(path);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ListFiles.Clear();
+            listView1.Items.Clear();
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog() { Description="Select your path"})
+            {
+                if(fbd.ShowDialog() == DialogResult.OK)
+                {
+                    textBox2.Text = fbd.SelectedPath;
+                    selectedPath = textBox2.Text;
+                    foreach (string item in Directory.GetFiles(fbd.SelectedPath))
+                    {https://mdps.rigasmezi.lv/?fbclid=IwAR3efpcIMTmtNu20qBQgHAZxuSXU81hKFafYJRwzXrs2MAbD_owx4n-afyM
+                        imageList1.Images.Add(System.Drawing.Icon.ExtractAssociatedIcon(item));
+                        FileInfo fileInfo = new FileInfo(item);
+                        ListFiles.Add(fileInfo.FullName);
+                        listView1.Items.Add(fileInfo.Name, imageList1.Images.Count - 1);
+                    }
+                }
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
